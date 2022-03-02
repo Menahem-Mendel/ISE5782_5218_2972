f07b5dfb784257a49c9ffea52d56b27e088d86a0
Josef Wolf <jwolf@g.jct.ac.il>
2022-03-02 18:34:22 +0200

    ex1

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,9 +1,9 @@
-package geometries;
+// package geometries;
 
-import primitives.Point;
+// import primitives.Point;
 
-public class Triangle extends Polygon {
-	public Triangle(Point p1, Point p2, Point p3) {
-		super(p1, p2, p3);
-	}
-}
+// public class Triangle extends Polygon {
+// 	public Triangle(Point p1, Point p2, Point p3) {
+// 		super(p1, p2, p3);
+// 	}
+// }
