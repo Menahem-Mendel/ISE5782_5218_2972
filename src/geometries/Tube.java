d63019cdaea5a7b4b5cdd30e0b999cb49e499b54
Josef Wolf <jwolf@g.jct.ac.il>
2022-03-06 20:03:25 +0200

    Tube

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,5 +1,38 @@
-// package geometries;
+package geometries;
 
-// public class Tube implements Geometry {
+/**
+ * Tube defines by a Ray and radius
+ */
+import primitives.*;
+public class Tube implements Geometry {
 
-// }
+    private final Ray axisRay;
+	private final double radius;
+
+    public Tube(Ray ray , double r){
+        axisRay=ray;
+        radius=r;
+    }
+
+    public Ray getAxisRay(){
+        return axisRay;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    
+    @Override
+    public Vector getNormal(Point p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+}
