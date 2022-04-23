cb7ee876f5d57b7bc5e3baa2a970a8b5c2831567
mendel <mendelgel@gmail.com>
2022-04-23 17:19:56 +0300

    files from Dan

---
+++ src/elements/AmbientLight.java
@@ -0,0 +1,39 @@
+package elements;
+
+import primitives.Color;
+import primitives.Double3;
+
+/**
+ * AmbientLight class
+ * 
+ */
+public class AmbientLight {
+	private Color intensity;
+
+	/**
+	 * AmbientLight build ctor
+	 * 
+	 * @param Ia original color of the light
+	 * @param Ka attenuation factor of the original light
+	 */
+	public AmbientLight(Color Ia, Double3 Ka) {
+		intensity = Ia.scale(Ka); // Ip = Ka * Ia
+	}
+
+	/**
+	 * AmbientLight empty build ctor sets the intensity to black
+	 * 
+	 */
+	public AmbientLight() {
+		intensity = Color.BLACK;
+	}
+
+	/**
+	 * getIntensity returns the intensity of the light
+	 * 
+	 * @return intensity of the light
+	 */
+	public Color getIntensity() {
+		return intensity;
+	}
+}
---
+++ src/scene/Scene.java
@@ -0,0 +1,35 @@
+package scene;
+
+import elements.AmbientLight;
+import geometries.Geometries;
+import primitives.Color;
+
+public class Scene {
+	public String name;
+	public Color background = Color.BLACK;
+	public AmbientLight ambientLight = new AmbientLight();
+	public Geometries geometries = new Geometries();
+
+	public Scene(String n) {
+		name = n;
+		geometries = new Geometries();
+	}
+
+	public Scene setBackground(Color bg) {
+		background = bg;
+
+		return this;
+	}
+
+	public Scene setAmbientLight(AmbientLight al) {
+		ambientLight = al;
+
+		return this;
+	}
+
+	public Scene setGeometries(Geometries gs) {
+		geometries = gs;
+
+		return this;
+	}
+}
